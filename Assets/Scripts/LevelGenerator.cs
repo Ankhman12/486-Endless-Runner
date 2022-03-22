a339484a15942fae44e6e07d54765b4cab778866
Mitchell Dunning <ankhman12@gmail.com>
2022-03-22 19:17:38 -0400

    Added level art

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -9,6 +9,10 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField] private Vector3 startPos;
     [SerializeField] private Transform levelPart_1;
     [SerializeField] private Transform levelPart_2;
+    [SerializeField] private Transform myelinD_1;
+    [SerializeField] private Transform myelinH_1;
+    [SerializeField] private Transform myelinD_2;
+    [SerializeField] private Transform myelinH_2;
 
     [SerializeField] private PlayerMovement player;
 
@@ -43,14 +47,31 @@ public class LevelGenerator : MonoBehaviour
         Transform levelPartTransform = null;
         Quaternion spawnRot = Quaternion.Euler(0f, 180f, Random.Range(0f, 360f));
         int partToSpawn = Random.Range(1, 2); // make 1-3 later
+        int myelinToSpawn = Random.Range(1, 10);
         switch (partToSpawn) {
             case 1:
                 spawnPos.z += 30;
                 levelPartTransform = Instantiate(levelPart_1, spawnPos, spawnRot);
-                break;
+                if (myelinToSpawn > 8)
+                {
+                    Instantiate(myelinD_1, spawnPos, spawnRot);
+                }
+                else 
+                {
+                    Instantiate(myelinH_1, spawnPos, spawnRot);
+                }
+                    break;
             case 2:
                 spawnPos.z += 25;
                 levelPartTransform = Instantiate(levelPart_2, spawnPos, spawnRot);
+                if (myelinToSpawn > 8)
+                {
+                    Instantiate(myelinD_2, spawnPos, spawnRot);
+                }
+                else
+                {
+                    Instantiate(myelinH_2, spawnPos, spawnRot);
+                }
                 break;
             case 3:
                 break;
