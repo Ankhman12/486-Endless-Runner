5b70618958f24711dfbd4596ecda2a6256f8d6f2
rnrice2 <rnrice2@ncsu.edu>
2022-03-22 01:25:04 -0400

    Began work on trigger for damage surfaces

--- Assets/Scripts/SymptomNumbness.cs
+++ Assets/Scripts/SymptomNumbness.cs
@@ -9,23 +9,51 @@ public class SymptomNumbness : MonoBehaviour
 {
     [SerializeField] GameObject postProcessor;
     [SerializeField] GameObject blackScreen;
+    [Range(0f, 1f), SerializeField] float initialVolumeWeight;
+    [Range(0f, 1f), SerializeField] float finalVolumeWeight;
+    [Range(0f, 1f), SerializeField] float initialOpacity;
 
     private Volume postProcessorVolume;
-    private float postProcessorWeight;
     private Image blackScreenImage;
+    private bool fading;
+    private Coroutine fadeCoroutine;
 
     void Start()
     {
         postProcessorVolume = postProcessor.GetComponent<Volume>();
-        postProcessorWeight = postProcessorVolume.weight;
         blackScreenImage = blackScreen.GetComponent<Image>();
+        resetShading();
     }
 
     void fadeToBlack()
     {
-        // Increase the weight of the post processor
-        postProcessorVolume.weight += 0.001f;
-        // Increase the alpha of the black screen
-        blackScreenImage.color = new Color(0, 0, 0, 0.001f + blackScreenImage.color.a);
+        if (postProcessorVolume.weight < finalVolumeWeight) {
+            // Increase the weight of the post processor
+            postProcessorVolume.weight += 0.00025f;
+            // Increase the alpha of the black screen
+            blackScreenImage.color = new Color(0, 0, 0, 0.00025f + blackScreenImage.color.a);
+        }
+        else {
+            fading = false;
+        }
+    }
+
+    void resetShading() 
+    {
+        fading = false;
+        postProcessorVolume.weight = initialVolumeWeight;
+        blackScreenImage.color = new Color(0, 0, 0, initialOpacity);
+    }
+
+    void Update() 
+    {   
+        // Check to see if the player is on a damaged tag
+        if (gameObject.CompareTag("Damaged")) {
+            // If the player is on a damaged tag and the post processor is not already fading, fade to black
+            if (!fading) {
+                fading = true;
+                fadeCoroutine = StartCoroutine("fadeToBlack");
+            }
+        }
     }
 }
