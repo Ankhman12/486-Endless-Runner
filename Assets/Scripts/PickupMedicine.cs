29524a4290482e5fda650462e402aaa2eee9b2eb
Ryan Rice <rnrice2@ncsu.edu>
2022-03-28 21:52:03 -0400

    Finally got the medicine pick up working

---
+++ Assets/Scripts/PickupMedicine.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupMedicine : MonoBehaviour
+{
+    [SerializeField] GameManager.Symptom symptomType;
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(this.gameObject);
+        GameManager.Instance.RemoveSymptom(symptomType);
+    }
+}
